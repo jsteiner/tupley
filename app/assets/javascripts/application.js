4dbf796952b0f19bf91a2694d75b241100eb33f5
Josh Steiner <josh@jsteiner.me>
2013-03-25 22:39:48 -0400

    Layout task index. Show checkboxes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require flat-ui-checkbox
 //= require_tree .
---
+++ vendor/assets/javascripts/flat-ui-checkbox.js
@@ -0,0 +1,34 @@
+// Custom checkbox
+function setupLabel() {
+    // Checkbox
+    var checkBox = ".checkbox";
+    var checkBoxInput = checkBox + " input[type='checkbox']";
+    var checkBoxChecked = "checked";
+    var checkBoxDisabled = "disabled";
+
+    // Checkboxes
+    if ($(checkBoxInput).length) {
+        $(checkBox).each(function(){
+            $(this).removeClass(checkBoxChecked);
+        });
+        $(checkBoxInput + ":checked").each(function(){
+            $(this).parent(checkBox).addClass(checkBoxChecked);
+        });
+        $(checkBoxInput + ":disabled").each(function(){
+            $(this).parent(checkBox).addClass(checkBoxDisabled);
+        });
+    };
+};
+
+$(document).ready(function(){
+    $("html").addClass("has-js");
+
+    // First let's prepend icons (needed for effects)
+    $(".checkbox").prepend("<span class='icon'></span><span class='icon-to-fade'></span>");
+
+    $(".checkbox").click(function(){
+        setupLabel();
+    });
+    setupLabel();
+});
+
